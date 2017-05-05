6425a996667e238ea904324bb2fb51409e02ec66
Caleb Cartwright <caleb.cartwright@outlook.com>
2017-05-04 22:40:05 -0500

    Initial commit with TestCategories defined

---
+++ Swellaby.TestAttributes/TestCategories.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Swellaby.TestAttributes
+{
+    /// <summary>
+    /// Constants to use consistent values on the TestCategory attribute used commonly in MSTest.
+    /// In tools such as TFS and VSTS, the TestCategory can be used to filter which tests are executed.
+    /// </summary>
+    public class TestCategories
+    {
+        public const string Unit = "Unit";
+        public const string ComponentIntegration = "Component Integration";
+        public const string SystemIntegration = "System Integration";
+        public const string Functional = "Functional";
+    }
+}
